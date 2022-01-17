8a297339f2a6b90b18e7bdd1724ae69ed3107ab3
Tommy <tbkknight1@icloud.com>
2022-01-17 12:20:47 -0600

    made a game module for the entire code, and then made a game board module that has a gameArray witch is just 9 zeros.

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+const game = (() => {
+    'use strict';
+    const gameBoard = (() => {
+        let gameArray = [
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0,
+        ];
+
+        const renderGameBoard = () => {
+            
+        }
+        return {
+            gameArray
+        };
+
+    })();
+    return {
+        gameBoard
+    };
+
+})();
