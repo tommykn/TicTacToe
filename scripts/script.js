bb8bb97c188cc96684c38967444e6678d89f3392
Tommy <tbkknight1@icloud.com>
2022-01-17 17:48:47 -0600

    made a displaycontoller moduls that updates the grid on what ever's inside the dom array.

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,49 @@
 const game = (() => {
     'use strict';
+    /* this will be a a factory function that creates the player object */
+    const Player = (name, type) => {
+
+        const makeMove = (player) => {
+
+
+
+        }
+
+    }
+
     const gameBoard = (() => {
         let gameArray = [
-            0, 0, 0,
-            0, 0, 0,
-            0, 0, 0,
+            1, 2, 3,
+            4, 7, 6,
+            7, 8, 9,
         ];
 
         const renderGameBoard = () => {
-            
+
         }
         return {
             gameArray
         };
 
     })();
+
+
+    const displayController = (() => {
+        const domArray = document.querySelectorAll('.grid-item');
+
+        for (let i = 0; i < domArray.length; i++) {
+            domArray[i].textContent = gameBoard.gameArray[i];
+        }
+
+        return {
+            domArray
+        };
+
+    })();
+
     return {
-        gameBoard
+        gameBoard,
+        displayController
     };
 
 })();
