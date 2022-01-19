1badbb0a811aafa0b816d57492789d3c27f9b738
Tommy <tbkknight1@icloud.com>
2022-01-19 14:15:10 -0600

    made a checkformove method in the game module, and returned it will use it in the displayContoller module.

--- scripts/script.js
+++ scripts/script.js
@@ -31,11 +31,56 @@ const code = (() => {
 
     const game = (() => {
         let turn = 'X';
+        // I will make a checkForWin method that I will used after every move.
+        const checkForWin = () => {
+            // check all horizontal lications
+            if (gameBoard.gameArray[0] === 'X' && gameBoard.gameArray[1] === 'X' && gameBoard.gameArray[2] === 'X') {
+                return true;
+            } else if (gameBoard.gameArray[0] === 'O' && gameBoard.gameArray[1] === 'O' && gameBoard.gameArray[2] === 'O') {
+                return true;
+            } else if (gameBoard.gameArray[3] === 'X' && gameBoard.gameArray[4] === 'X' && gameBoard.gameArray[5] === 'X') {
+                return true;
+            } else if (gameBoard.gameArray[3] === 'O' && gameBoard.gameArray[4] === 'O' && gameBoard.gameArray[5] === 'O') {
+                return true;
+            } else if (gameBoard.gameArray[6] === 'O' && gameBoard.gameArray[7] === 'O' && gameBoard.gameArray[8] === 'O') {
+                return true;
+            } else if (gameBoard.gameArray[6] === 'X' && gameBoard.gameArray[7] === 'X' && gameBoard.gameArray[8] === 'X') {
+                return true;
+                // check for all vertical wins
+            } else if (gameBoard.gameArray[0] === 'O' && gameBoard.gameArray[3] === 'O' && gameBoard.gameArray[6] === 'O') {
+                return true;
+            } else if (gameBoard.gameArray[0] === 'X' && gameBoard.gameArray[3] === 'X' && gameBoard.gameArray[6] === 'X') {
+                return true;
+            } else if (gameBoard.gameArray[1] === 'X' && gameBoard.gameArray[4] === 'X' && gameBoard.gameArray[7] === 'X') {
+                return true;
+            } else if (gameBoard.gameArray[1] === 'O' && gameBoard.gameArray[4] === 'O' && gameBoard.gameArray[7] === 'O') {
+                return true;
+            } else if (gameBoard.gameArray[2] === 'O' && gameBoard.gameArray[5] === 'O' && gameBoard.gameArray[8] === 'O') {
+                return true;
+            } else if (gameBoard.gameArray[2] === 'X' && gameBoard.gameArray[5] === 'X' && gameBoard.gameArray[8] === 'X') {
+                return true;
+                // check all diaganal wins by using if statements.
+            } else if (gameBoard.gameArray[0] === 'O' && gameBoard.gameArray[4] === 'O' && gameBoard.gameArray[8] === 'O') {
+                return true;
+            } else if (gameBoard.gameArray[0] === 'X' && gameBoard.gameArray[4] === 'X' && gameBoard.gameArray[8] === 'X') {
+                return true;
+            } else if (gameBoard.gameArray[2] === 'O' && gameBoard.gameArray[4] === 'O' && gameBoard.gameArray[6] === 'O') {
+                return true;
+            } else if (gameBoard.gameArray[2] === 'X' && gameBoard.gameArray[4] === 'X' && gameBoard.gameArray[6] === 'X') {
+                return true;
+            } else {
+                return false;
+            }
+
+
+
+        };
 
 
 
         return {
-            turn
+            turn,
+            checkForWin
         };
 
     })();
@@ -55,6 +100,9 @@ const code = (() => {
                 game.turn = 'O';
             }
             updateGameBoard();
+            if (checkForMoves()) {
+                
+            }
         };
 
 
